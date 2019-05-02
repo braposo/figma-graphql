87e465130840c7995ed3a7bf1b3ae1ad5803216b
Bernardo Raposo <hi@bernardoraposo.com>
2019-05-02 15:35:41 +0100

    refactor: remove subscriptions for now

--- src/types/file.js
+++ src/types/file.js
@@ -1,8 +1,5 @@
 const { gql } = require("apollo-server-express");
-const { PubSub } = require("apollo-server-express");
 const { loadFigma, getChildren } = require("../utils");
-
-const pubsub = new PubSub();
 
 exports.type = gql`
     # Information about a file
@@ -31,44 +28,17 @@ exports.type = gql`
     }
 `;
 
-const FILE_MODIFIED = "FILE_MODIFIED";
-let poller;
-let currentModified;
-
-async function handlePoller(id, prevModified) {
-    const data = await loadFigma(id);
-    const { lastModified } = data;
-    if (prevModified !== lastModified) {
-        console.log(`Trigger update! ${lastModified}`);
-        currentModified = lastModified;
-        pubsub.publish(FILE_MODIFIED, { file: { ...data } });
-    }
-}
-
 exports.resolvers = {
     Query: {
-        file: async (root, { id }) => {
-            // Trigger the polling
-            const data = await loadFigma(id);
-            if (poller) {
-                clearInterval(poller);
-            }
-            poller = setInterval(() => handlePoller(id, currentModified), 5000);
-            return data;
-        },
+        file: (root, { id }) => loadFigma(id).then(data => data),
     },
     File: {
         pages: (root, { name }) => {
             if (name) {
-                return getChildren(root, "document.children", { name });
+                return getChildren(root.document, "children", { name });
             }
 
-            return getChildren(root, "document.children");
-        },
-    },
-    Subscription: {
-        file: {
-            subscribe: () => pubsub.asyncIterator([FILE_MODIFIED]),
+            return getChildren(root.document, "children");
         },
     },
 };
