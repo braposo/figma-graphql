67612fc4e7493369feffbedffbd2490761f61e6f
Bernardo Raposo <hi@bernardoraposo.com>
2019-05-05 23:03:18 +0100

    feat: add docs (#14)
    * chore: add packages for docs
    
    * chore: remove gatsby and use docz
    
    * chore: add docz config file
    
    * docs: add initial docs
    
    * ci(travis): update config to publish docs on each release

---
+++ doczrc.js
@@ -0,0 +1,11 @@
+export default {
+    title: "figma-graphql",
+    description: "The reimagined Figma API (super)powered by GraphQL",
+    src: "./docs",
+    themeConfig: {
+        colors: {
+            link: "#1ABCFE",
+            primary: "#FF7262",
+        },
+    },
+};
