5f715d340ee15e8896940652d9db5cb8460cdc34
Bernardo Raposo <hi@bernardoraposo.com>
2019-05-25 09:28:32 +0100

    refactor: updates server to include file id in context

--- src/main.js
+++ src/main.js
@@ -1,31 +1,35 @@
 require("dotenv").config();
 
-const http = require("http");
 const cors = require("cors");
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 
-const { loadFigma } = require("./utils");
+const { loadFigmaFile } = require("./utils/figma");
+const { getFileId } = require("./utils/helpers");
 const { schema } = require("./schema");
 
 const PORT = 3001;
 
-const server = new ApolloServer({ schema });
+const server = new ApolloServer({
+    schema,
+    context: async ({ req }) => ({
+        fileId: getFileId(req.body.query),
+    }),
+});
 
 const app = express();
 app.use(cors());
 server.applyMiddleware({ app });
 
-const httpServer = http.createServer(app);
-
 // Get figma API response (just for testing)
 app.get("/figma/:id", (req, res) => {
     const { id } = req.params;
-    loadFigma(id).then(data => {
+    loadFigmaFile(id).then(data => {
         res.json(data);
     });
 });
 
-httpServer.listen(PORT, () => {
+app.listen(PORT, () => {
+    // eslint-disable-next-line no-console
     console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
 });
