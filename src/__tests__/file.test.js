e9432d2507d7a4b40ba3610225fb351ffe8f4aca
Bernardo Raposo <hi@bernardoraposo.com>
2019-05-26 19:37:54 +0100

    test: add tests to comments in file

--- src/__tests__/file.test.js
+++ src/__tests__/file.test.js
@@ -35,4 +35,54 @@ describe("File", () => {
             data: { file: { images: [{ id: "0:1" }] } },
         });
     });
+
+    test("can get file comments", async () => {
+        const query = `
+            query {
+                file(id: "${figmaFile}") {
+                    comments {
+                        id
+                        message
+                        file_key
+                        order_id
+                        parent_id
+                        created_at
+                        client_meta {
+                                  node_id
+                          node_offset {
+                            x
+                            y
+                          }
+                        }
+                        resolved_at
+                      }
+                }
+            }
+        `;
+
+        await expect(graphql(schema, query, null, { fileId: figmaFile })).resolves.toEqual({
+            data: {
+                file: {
+                    comments: [
+                        {
+                            id: "6907475",
+                            message: "Testing comment",
+                            file_key: "cLp23bR627jcuNSoBGkhL04E",
+                            order_id: 1,
+                            parent_id: "",
+                            created_at: "2019-05-26T17:47:36Z",
+                            client_meta: {
+                                node_id: "28:5",
+                                node_offset: {
+                                    x: 111,
+                                    y: 22,
+                                },
+                            },
+                            resolved_at: null,
+                        },
+                    ],
+                },
+            },
+        });
+    });
 });
