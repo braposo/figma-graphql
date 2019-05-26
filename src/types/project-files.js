46bc8fdbc3a394fd60097c91632f01fbc2d1d18b
Bernardo Raposo <hi@bernardoraposo.com>
2019-05-27 00:16:08 +0100

    feat: stitch teams, projects and files together

--- src/types/project-files.js
+++ src/types/project-files.js
@@ -1,26 +1,20 @@
-const { loadProjectFiles } = require("../utils/figma");
+const { loadProjectFiles, loadFile } = require("../utils/figma");
 
 exports.type = `
-    # A file inside a project
-    type ProjectFile {
-     # Key of this file
-     key: String
-     # Name of the file
-     name: String
-     # Thumbnail of the file
-     thumbnail_url: String
-     # Date when last modified
-     last_modified: String
-    }
-
     extend type Query {
         # Get a projects files
-        projectFiles(project: String!): [ProjectFile]
+        projectFiles(projectId: String!): [File]
     }
 `;
 
 exports.resolvers = {
     Query: {
-        projectFiles: (root, { project }) => loadProjectFiles(project).then(data => data.files),
+        projectFiles: async (_, { projectId }) => {
+            const { files: projectFiles } = await loadProjectFiles(projectId);
+
+            const parsedFiles = await Promise.all(projectFiles.map(({ key }) => loadFile(key)));
+
+            return Object.values(parsedFiles);
+        },
     },
 };
