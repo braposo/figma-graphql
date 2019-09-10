336691bd39b1b2d10bde776f7cc79f52a60293e4
Bernardo Raposo <hi@bernardoraposo.com>
2019-09-10 23:16:00 +0100

    refactor(ts): improve types for groupNodes

--- src/utils/nodes.ts
+++ src/utils/nodes.ts
@@ -2,7 +2,8 @@ import groupBy from "lodash/groupBy";
 import uniqBy from "lodash/uniqBy";
 import { Node } from "figma-js";
 
-export const groupNodes = (nodes: Node[]) => groupBy(uniqBy(nodes, "id"), "type");
+export const groupNodes = (nodes: Node[]): Record<string, Node[]> =>
+    groupBy(uniqBy(nodes, "id"), "type");
 
 export const getNodes = (data, nodeType, filterBy) => {
     const { name: filterByName, type: filterByType } = filterBy;
