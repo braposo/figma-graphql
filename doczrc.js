b14680ba6a1c6843e8edaa6f58f34766930dab9f
Bernardo Raposo <braposo@gmail.com>
2020-04-11 15:42:52 +0100

    docs: set src folder for docz

--- doczrc.js
+++ doczrc.js
@@ -1,4 +1,6 @@
+// eslint-disable-next-line import/no-default-export
 export default {
+    src: "./docs",
     title: "figma-graphql",
     description: "The reimagined Figma API (super)powered by GraphQL",
     themeConfig: {
